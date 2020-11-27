4beeeef68d6559766ed2e64392ce97f067a3d379
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-27 19:23:11 +0530

    [sasic] Add . Adding Contacts inTo AddressBook Table

--- AddressBookLINQ/AddressBookRepo.cs
+++ AddressBookLINQ/AddressBookRepo.cs
@@ -8,10 +8,13 @@ namespace AddressBookLINQ
     public class AddressBookRepo
     {
         /// <summary>
+        /// UC1
         /// Initializes a new instances of the data table
         /// </summary>
         DataTable dataTable = new DataTable();
+
         /// <summary>
+        /// UC2
         /// Creates the book table.
         /// </summary>
         public void CreateBookTable()
@@ -25,6 +28,15 @@ namespace AddressBookLINQ
             dataTable.Columns.Add("ZipCode", typeof(string));
             dataTable.Columns.Add("PhoneNumber", typeof(string));
             dataTable.Columns.Add("Email", typeof(string));
+
+            /// UC2
+            /// Creating rows and inserting in column vice
+            dataTable.Rows.Add("Ravi","Kumar","Gowtham Nagar","Bangarpet","Karnataka","8073112156","shashidhar.sasic@gmail.com");
+            dataTable.Rows.Add("Lalith", "Kumar", "Vijay Nagar", "Bangarpet", "Karnataka", "8223112156", "lalith.lalu@gmail.com");
+            dataTable.Rows.Add("Mohan", "Kumar", "Shanthi Nagar", "Bangalore", "Tamilnadu", "9973112156", "mohanrah.mohan@gmail.com");
+            dataTable.Rows.Add("Kiran", "Kumar", "Ambedkar Nagar", "Kolar", "AndhraPradesh", "8073174156", "kiran.Kid@gmail.com");
+            dataTable.Rows.Add("Sridhar", "Kumar", "Amravathi Nagar", "Kgf", "Delhi", "8073114656", "sridhar.sri@gmail.com");
+            dataTable.Rows.Add("Praveen", "Kumar", "Vivekanandha Nagar", "Malur", "Mumbai", "8000112156", "praveen.pravi@gmail.com");
         }
     }
 }
