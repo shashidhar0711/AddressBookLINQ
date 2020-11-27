edbcd37526fd3d347ca4870afdcf5b7324198a3b
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-27 18:58:04 +0530

    [sasic] Add . Able to Create Address Book Table

---
+++ AddressBookLINQ/AddressBookRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace AddressBookLINQ
+{
+    public class AddressBookRepo
+    {
+        /// <summary>
+        /// Initializes a new instances of the data table
+        /// </summary>
+        DataTable dataTable = new DataTable();
+        /// <summary>
+        /// Creates the book table.
+        /// </summary>
+        public void CreateBookTable()
+        {
+            /// Creating columns
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("ZipCode", typeof(string));
+            dataTable.Columns.Add("PhoneNumber", typeof(string));
+            dataTable.Columns.Add("Email", typeof(string));
+        }
+    }
+}
--- AddressBookLINQ/Program.cs
+++ AddressBookLINQ/Program.cs
@@ -4,9 +4,15 @@ namespace AddressBookLINQ
 {
     public class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Welocme To AddressBook Using Linq");
+            AddressBookRepo addressBookRepo = new AddressBookRepo();
+            addressBookRepo.CreateBookTable();
         }
     }
 }
