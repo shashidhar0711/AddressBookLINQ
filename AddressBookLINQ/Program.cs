779e3ec3a8f921fc755ee0883b3a465e1cf160a9
shashidhar0711 <shashidhar.sasic@gmail.com>
2020-11-27 18:42:55 +0530

    Add project files.

---
+++ AddressBookLINQ/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookLINQ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welocme To AddressBook Using Linq");
+        }
+    }
+}
